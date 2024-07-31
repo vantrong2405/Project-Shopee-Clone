e55d5bdff04079d9116ce49e374b703bf6b00531
Van trong <trongdn2405@gmail.com>
2024-07-31 19:25:27 +0700

    01. Init config project shopee clone

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname)],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
+  }
+};
