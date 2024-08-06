7fcfa43b9ee6ba4cf519980fe3891cbddf910658
Van trong <trongdn2405@gmail.com>
2024-08-06 20:02:17 +0700

    08. Config class container trong tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4'),
+          paddingRight: theme('spacing.4')
+        }
+      })
+    }),
+  ]
 }
 
