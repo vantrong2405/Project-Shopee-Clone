2f3eb8803d9588465a0653865bc684abd7d5ad8b
Van trong <trongdn2405@gmail.com>
2024-08-07 17:39:46 +0700

    12. Xây dựng router protected và reject để bảo vệ login

--- src/useRouterElement.tsx
+++ src/useRouterElement.tsx
@@ -1,34 +1,56 @@
 import React from 'react'
-import { useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 import ProductList from './pages/ProductList';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import RegisterLayout from './layouts/RegisterLayout';
 import MainLayout from './layouts/MainLayout';
 
+const authenicated = true
+const protectedAuthenicated = () => {
+  return authenicated ? <Outlet /> : <Navigate to='/login' />
+}
+
+const rejectAuthenicated = () => {
+  return !authenicated ? <Outlet /> : <Navigate to='/' />
+}
+
 export default function useRouterElement() {
   let routerElement = useRoutes([
     {
-      path: '/',
-      element:
-        <MainLayout>
-          <ProductList />
-        </MainLayout>
+      path: '',
+      element: protectedAuthenicated(),
+      children: [
+        {
+          path: '/',
+          element:
+            <MainLayout>
+              <ProductList />
+            </MainLayout>
+        },
+      ]
     },
     {
-      path: '/login',
-      element:
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-    },
-    {
-      path: '/register',
-      element:
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
+      path: '',
+      element: rejectAuthenicated(),
+      children: [
+        {
+          path: '/login',
+          element:
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout >
+        },
+        {
+          path: '/register',
+          element:
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+        },
+      ]
     }
+
   ]);
   return routerElement
 }
