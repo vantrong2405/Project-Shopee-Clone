796da96959a73f0dd62e9000bcc8ffcfadab868a
Van trong <trongdn2405@gmail.com>
2024-08-10 21:10:23 +0700

    19. Xây dựng components rating và logic rating

--- src/Components/Product/Product.tsx
+++ src/Components/Product/Product.tsx
@@ -5,6 +5,7 @@ import productApi from 'src/apis/product.api'
 import useQueryParams from 'src/hook/useQueryParams'
 import { Product as ProductType } from 'src/type/product.type'
 import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
+import ProductRating from '../ProductRating'
 
 interface Props {
   product: ProductType
@@ -35,40 +36,7 @@ export default function Product({ product }: Props) {
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg
-                    enableBackground='new 0 0 15 15'
-                    viewBox='0 0 15 15'
-                    x={0}
-                    y={0}
-                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
-                  >
-                    <polygon
-                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeMiterlimit={10}
-                    />
-                  </svg>
-                </div>
-                <svg
-                  enableBackground='new 0 0 15 15'
-                  viewBox='0 0 15 15'
-                  x={0}
-                  y={0}
-                  className='w-3 h-3 fill-current text-gray-300'
-                >
-                  <polygon
-                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeMiterlimit={10}
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
               <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
