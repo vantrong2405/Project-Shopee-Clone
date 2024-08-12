ef733c38e1a118390036f1e59263872aafc302b6
Van trong <trongdn2405@gmail.com>
2024-08-12 18:08:50 +0700

    22. Hoàn thiện logic SortProductList

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
-const path = {
+  const path = {
   home: '/',
   login: '/login',
   register: '/register',
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -42,7 +42,7 @@ export default function ProductList() {
               <AsideFilter />
             </div>
             <div className='col-span-9'>
-              <SortProductList />
+              <SortProductList queryConfig={queryConfig} pageSize={data?.data.data.pagination.page_size} />
               <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
                 {data.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
