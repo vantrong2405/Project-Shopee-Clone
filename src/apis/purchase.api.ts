2c7f78a703196de74094e09ac5ce71467840001e
Van trong <trongdn2405@gmail.com>
2024-08-13 21:58:01 +0700

    33. Khai báo api purchase

---
+++ src/apis/purchase.api.ts
@@ -0,0 +1,18 @@
+import { Purchase, PurchaseListStatus } from 'src/type/purchase.type'
+import { SuccessResponse } from 'src/type/utils.type'
+import http from 'src/utils/http'
+
+const URL = 'purchases'
+
+const purchaseApi = {
+  addToCart(body: { product_id: string; buy_count: number }) {
+    return http.post<SuccessResponse<Purchase>>(`${URL}/add-to-cart`, body)
+  },
+  getPurchases(params: { status: PurchaseListStatus }) {
+    return http.get<SuccessResponse<Purchase[]>>(`${URL}`, {
+      params
+    })
+  }
+}
+
+export default purchaseApi
