0215a6d95f394fadd1c7fab9bae556e7aea53a8e
Van Trong <trongdn2405@gmail.com>
2024-09-12 14:47:27 +0700

    47. Sửa components dataselect và thực hiện việc update profile

--- src/pages/User/components/UserSideNav/UserSideNav.tsx
+++ src/pages/User/components/UserSideNav/UserSideNav.tsx
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom'
 import path from 'src/constants/path'
-
+import { useContext } from 'react'
+import { themeContext } from 'src/context/app.context'
+import userImage from '../../../../assets/images/user.png'
 export default function UserSideNav() {
+  const { profile } = useContext(themeContext)
   return (
     <div>
       <div className='flex items-center border-b border-b-gray-200 py-4'>
         <Link to={path.profile} className='h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-black/10'>
-          <img
-            src='https://cf.shopee.vn/file/d04ea22afab6e6d250a370d7ccc2e675_tn'
-            alt=''
-            className='h-full w-full object-cover'
-          />
+        <img src={profile?.avatar || userImage} alt='' className='h-full w-full object-cover' />
         </Link>
         <div className='flex-grow pl-4'>
-          <div className='mb-1 truncate font-semibold text-gray-600'>cdthanh</div>
+        <div className='mb-1 truncate font-semibold text-gray-600'>{profile?.email}</div>
           <Link to={path.profile} className='flex items-center capitalize text-gray-500'>
             <svg
               width={12}
