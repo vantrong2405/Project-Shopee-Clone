8b9351f8aebb5ddd694488748d3f699c02814c73
vtrong <trongdn2405@gmail.com>
2024-11-12 16:31:43 +0700

    53. Áp dụng refresh_token khi access_token hết hạn

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,25 +1,33 @@
 import axios, { type AxiosInstance } from 'axios'
 import { HttpStatusCode } from 'src/constants/httpStatusCode.enum'
-import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {
   clearLS,
   getAccessTokenFromLS,
+  getRefreshTokenFromLS,
   setAccessTokenToLS,
-  setProfileToLS
+  setProfileToLS,
+  setRefreshTokenToLS
 } from './auth'
 import path from 'src/constants/path'
 import config from 'src/constants/config'
+import { RefreshTokenResponse } from 'src/type/auth.type'
+import { toast } from 'react-toastify'
+import { isAxiosExpiredTokenError } from 'src/utils/utils'
 class Http {
   instance: AxiosInstance
   private accessToken: string
+  private refreshToken: string
   constructor() {
     this.accessToken = getAccessTokenFromLS()
+    this.refreshToken = getRefreshTokenFromLS()
     this.instance = axios.create({
       baseURL: config.baseUrl,
       timeout: 1000 * 10,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'expire-access-token':120 * 60, // 1 =  1s
+        // 'expire-refresh-token': 60 * 60 // 1h
       }
     })
     this.instance.interceptors.request.use(
@@ -31,33 +39,50 @@ class Http {
         return Promise.reject(error)
       }
     )
-
+ 
     this.instance.interceptors.response.use(
       (response) => {
-        const { url } = response.config
-        const data = response.data
+        const { url } = response.config;
+        const data = response.data;
         if (url === path.login || url === path.register) {
-          this.accessToken = data.data.access_token
-          setAccessTokenToLS(this.accessToken)
-          setProfileToLS(data.data.user)
+          this.accessToken = data.data.access_token;
+          this.refreshToken = data.data.refresh_token;
+          setAccessTokenToLS(this.accessToken);
+          setRefreshTokenToLS(this.refreshToken);
+          setProfileToLS(data.data.user);
         } else if (url === path.logout) {
           this.accessToken = ''
-          clearLS()
+          this.refreshToken = ''
+          clearLS();
         }
         return response
       },
-      function (error) {
+      async (error) => {
         if (error?.response.status !== HttpStatusCode.UnprocessableEntity) {
-          const data: any | undefined = error.response?.data
-          const message = data?.message || error.message
-          // toast.error(message)
+          const data: any | undefined = error.response?.data;
+          const message = data?.message || error.message;
+          toast.error(message)
         }
         if (error?.response.status === HttpStatusCode.Unauthorized) {
+          if (isAxiosExpiredTokenError(error)) {
+            const newAccessToken = await this.handleRefreshToken()
+            if (newAccessToken) {
+              setAccessTokenToLS(newAccessToken)
+              return Promise.reject(error)
+            }
+          }
           clearLS()
         }
         return Promise.reject(error)
       }
-    )
+    );
+  }
+
+  private handleRefreshToken = async (): Promise<string | undefined> => {
+      const res = await this.instance.post<RefreshTokenResponse>('/refresh-access-token', {
+        refresh_token: this.refreshToken,
+      })
+      return res.data.data.access_token 
   }
 }
 
