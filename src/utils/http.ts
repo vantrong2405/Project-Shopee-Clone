7ff4ede41fb2331b081c677e1f8bd2d5eea05a7c
vtrong <trongdn2405@gmail.com>
2025-01-11 22:18:42 +0700

    Close notifice toast error

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -50,7 +50,7 @@ class Http {
         if (error?.response.status !== HttpStatusCode.UnprocessableEntity) {
           const data: any | undefined = error.response?.data
           const message = data?.message || error.message
-          toast.error(message)
+          // toast.error(message)
         }
         if (error?.response.status === HttpStatusCode.Unauthorized) {
           clearLS()
